fb912a8d07f27c1f90ab99522fd65535317eab18
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-21 10:10:45 +0900

    demo file update

--- diffusion_det/demo_sy.py
+++ diffusion_det/demo_sy.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
                         metavar="FILE", help="path to config file",)
     parser.add_argument("--webcam", action="store_true", help="Take inputs from webcam.")
     parser.add_argument("--video-input", help="Path to video file.")
-    parser.add_argument("--input", nargs="+",
+    parser.add_argument("--input", nargs="+", type=str,
                         help="A list of space separated input images; or a single glob pattern such as 'directory/*.jpg'",)
     parser.add_argument("--output",
                         help="A file or directory to save output visualizations. If not given, will show output in an OpenCV window.",)
